ac71feb334dc46cbbca734a0d0700a77b952757c
Ravid Cohen <ravidd9@gmail.com>
2019-06-22 20:46:10 +0300

    home

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Test from './components/Test';
+import Home from './components/Home';
 
 
 class App extends Component {
   render() {
     return (
       <div id="app">
-        <Test />
+        <Home />
       </div>
     );
   }
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Test from './Test';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <Test />
+            </div>
+        );
+    }
+}
+
+export default Home;
